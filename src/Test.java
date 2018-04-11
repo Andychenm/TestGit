6c7f1d16db93d82fa2473414078e45baa33b58e3
Andychenm <chenm990@163.com>
2018-04-11 13:27:30 +0800

    修改test.java文件

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public  static  void  main(String[ ] args){
-        System.out.println("=====test helloworld");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("=====test helloworld");
+        }
     }
 }
